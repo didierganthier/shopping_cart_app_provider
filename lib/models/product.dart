625b9e317e3fe0c4668fa324715b1fce7105ed18
Didier Peran Ganthier <didierganthierperan@gmail.com>
2025-01-28 09:47:40 -0500

    first commit

---
+++ lib/models/product.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class Product {
+  String id;
+  String name;
+  double price;
+  String imageUrl;
+
+  Product({
+    required this.id,
+    required this.name,
+    required this.price,
+    required this.imageUrl,
+  });
+}
